fc087120c21e55793bc60bf20fe62a16ccca804a
Shimakage <shimakage@gclue.jp>
2016-04-13 09:05:36 +0900

    add comment

--- src/FaBoRTC_PCF2129.cpp
+++ src/FaBoRTC_PCF2129.cpp
@@ -1,22 +1,48 @@
-#include "FaBoRTC_PCF2129.h"
+/**
+ * @file  FaBoRTC_PCF2129.cpp
+ * @brief fabo libtary of PCF2129
+ * @author Akira Sasaki
+ * @date 2,10, 2016
+ */
+ #include "FaBoRTC_PCF2129.h"
 
+ /**
+  * @brief PCF2129 processing Start
+  * @param addr : PCV2129 Slave Address
+  */
 FaBoRTC_PCF2129::FaBoRTC_PCF2129(uint8_t addr) {
   _i2caddr = addr;
   Wire.begin();
 }
 
+/**
+ * @brief Search Device PCF2129
+ * @retval true  : Found
+ * @retval false : Not found
+ */
 bool FaBoRTC_PCF2129::searchDevice(void) {
   return !(readI2c(_i2caddr) >>7);
 }
 
+/**
+ * @brief Set Config
+ */
 void FaBoRTC_PCF2129::configure(void) {
   set24mode();
 }
 
+/**
+ * @brief Get seconds from PCF2129
+ * @return uint8_t : get seconds
+ */
 uint8_t FaBoRTC_PCF2129::getSeconds(void) {
   return bcdToDec(readI2c(PCF2129_SECONDS));
 }
 
+/**
+ * @brief Set seconds to PCF2129
+ * @param seconds : Set to seconds
+ */
 void FaBoRTC_PCF2129::setSeconds(uint8_t seconds) {
   if ( seconds>59 && seconds<0 ) {
     seconds = 0;
@@ -24,10 +50,18 @@ void FaBoRTC_PCF2129::setSeconds(uint8_t seconds) {
   writeI2c(PCF2129_SECONDS, (decToBcd(seconds) + 0x80));
 }
 
+/**
+ * @brief Get minutes from PCF2129
+ * @return uint8_t : Get minutes
+ */
 uint8_t FaBoRTC_PCF2129::getMinutes(void) {
   return bcdToDec(readI2c(PCF2129_MINUTES));
 }
 
+/**
+ * @brief Set minutes to PCF2129
+ * @param [in] minutes : Set to minutes
+ */
 void FaBoRTC_PCF2129::setMinutes(uint8_t minutes) {
   if ( minutes>59 && minutes<0 ) {
     minutes = 0;
@@ -35,10 +69,18 @@ void FaBoRTC_PCF2129::setMinutes(uint8_t minutes) {
   writeI2c(PCF2129_MINUTES, decToBcd(minutes));
 }
 
+/**
+ * @brief Get hours from PCF2129
+ * @return uint8_t : Get hours
+ */
 uint8_t FaBoRTC_PCF2129::getHours(void) {
   return bcdToDec(readI2c(PCF2129_HOURS));
 }
 
+/**
+ * @brief Set hours to PCF2129
+ * @param [in] hours : Set to hours
+ */
 void FaBoRTC_PCF2129::setHours(uint8_t hours) {
   set24mode();
   if ( hours>23 && hours<0 ) {
@@ -47,10 +89,18 @@ void FaBoRTC_PCF2129::setHours(uint8_t hours) {
   writeI2c(PCF2129_HOURS, decToBcd(hours));
 }
 
+/**
+ * @brief Get days from PCF2129
+ * @return uint8_t : Get days
+ */
 uint8_t FaBoRTC_PCF2129::getDays(void) {
   return bcdToDec(readI2c(PCF2129_DAYS));
 }
 
+/**
+ * @brief Set days to PCF2129
+ * @param [in] days : Set to days
+ */
 void FaBoRTC_PCF2129::setDays(uint8_t days) {
   if ( days>31 && days<1 ) {
     days = 1;
@@ -58,10 +108,18 @@ void FaBoRTC_PCF2129::setDays(uint8_t days) {
   writeI2c(PCF2129_DAYS, decToBcd(days));
 }
 
+/**
+ * @brief Get weekdays from PCF2129
+ * @return uint8_t : Get weekdays
+ */
 uint8_t FaBoRTC_PCF2129::getWeekdays(void) {
   return bcdToDec(readI2c(PCF2129_WEEKDAYS));
 }
 
+/**
+ * @brief Set weekdays to PCF2129
+ * @param [in] weekdays : Set to weekdays
+ */
 void FaBoRTC_PCF2129::setWeekdays(uint8_t weekdays) {
   if ( weekdays>6 && weekdays<0 ) {
     weekdays = 0;
@@ -69,10 +127,18 @@ void FaBoRTC_PCF2129::setWeekdays(uint8_t weekdays) {
   writeI2c(PCF2129_WEEKDAYS, decToBcd(weekdays));
 }
 
+/**
+ * @brief Get months from PCF2129
+ * @return uint8_t : Get months
+ */
 uint8_t FaBoRTC_PCF2129::getMonths(void) {
   return bcdToDec(readI2c(PCF2129_MONTHS));
 }
 
+/**
+ * @brief Set months to PCF2129
+ * @param [in] months : Set to months
+ */
 void FaBoRTC_PCF2129::setMonths(uint8_t months) {
   if ( months>12 && months<1 ) {
     months = 1;
@@ -80,10 +146,18 @@ void FaBoRTC_PCF2129::setMonths(uint8_t months) {
   writeI2c(PCF2129_MONTHS, decToBcd(months));
 }
 
+/**
+ * @brief Get years from PCF2129
+ * @return uint8_t : Get years
+ */
 uint8_t FaBoRTC_PCF2129::getYears(void) {
   return bcdToDec(readI2c(PCF2129_YEARS));
 }
 
+/**
+ * @brief Set years to PCF2129
+ * @param [in] days : Set to years
+ */
 void FaBoRTC_PCF2129::setYears(uint8_t years) {
   if ( years>99 && years<0 ) {
     years = 0;
@@ -91,6 +165,9 @@ void FaBoRTC_PCF2129::setYears(uint8_t years) {
   writeI2c(PCF2129_YEARS, decToBcd(years));
 }
 
+/**
+ * @brief Get set to variable now from PCF2129
+ */
 DateTime FaBoRTC_PCF2129::now(void) {
   Wire.beginTransmission(_i2caddr);
   Wire.write(PCF2129_SECONDS);
@@ -108,6 +185,15 @@ DateTime FaBoRTC_PCF2129::now(void) {
   return DateTime(years, months, days, hours, minutes, seconds);
 }
 
+/**
+ * @brief Set Date from PCF2129
+ * @param [in] years   : Set Years Data
+ * @param [in] months  : Set months Data
+ * @param [in] days    : Set days Data
+ * @param [in] hours   : Set hours Data
+ * @param [in] minutes : Set minutes Data
+ * @param [in] seconds : Set seconds Data
+ */
 void FaBoRTC_PCF2129::setDate(uint16_t years, uint8_t months, uint8_t days,
                               uint8_t hours, uint8_t minutes, uint8_t seconds) {
   Wire.beginTransmission(_i2caddr);
@@ -122,6 +208,9 @@ void FaBoRTC_PCF2129::setDate(uint16_t years, uint8_t months, uint8_t days,
   Wire.endTransmission();
 }
 
+/**
+ * @brief Set 12-Hour Mode
+ */
 void FaBoRTC_PCF2129::set12mode(void) {
   uint8_t ctrl;
   ctrl = readCtrl();
@@ -129,6 +218,9 @@ void FaBoRTC_PCF2129::set12mode(void) {
   writeCtrl(ctrl);
 }
 
+/**
+ * @brief Set 24-Hour Mode
+ */
 void FaBoRTC_PCF2129::set24mode(void) {
   uint8_t ctrl;
   ctrl = readCtrl();
@@ -138,14 +230,29 @@ void FaBoRTC_PCF2129::set24mode(void) {
 
 ////////////////////////////////////////////////////////////////
 
+/**
+ * @brief Conversion from BCD to DEC
+ * @param  [in] value   : BCD Data
+ * @return uint8_t : DEC Data
+ */
 uint8_t FaBoRTC_PCF2129::bcdToDec(uint8_t value) {
   return (uint8_t) ( (value/16*10) + (value%16) );
 }
 
+/**
+ * @brief Conversion from BCD to DEC
+ * @param  [in] value   : DEC Data
+ * @return uint8_t : BCD Data
+ */
 uint8_t FaBoRTC_PCF2129::decToBcd(uint8_t value) {
   return (uint8_t) ( (value/10*16) + (value%10) );
 }
 
+/**
+ * @brief Read I2C Data
+ * @param [in] register_addr : register address
+ * @return uint8_t : Read Data
+ */
 uint8_t FaBoRTC_PCF2129::readI2c(uint8_t address) {
   Wire.beginTransmission(_i2caddr);
   Wire.write(address);
@@ -155,6 +262,11 @@ uint8_t FaBoRTC_PCF2129::readI2c(uint8_t address) {
   return Wire.read();
 }
 
+/**
+ * @brief Write I2C Data
+ * @param [in] address : Write Register Address
+ * @param [in] data    : Write Data
+ */
 void FaBoRTC_PCF2129::writeI2c(uint8_t address, uint8_t data) {
   Wire.beginTransmission(_i2caddr);
   Wire.write(address);
@@ -162,10 +274,18 @@ void FaBoRTC_PCF2129::writeI2c(uint8_t address, uint8_t data) {
   Wire.endTransmission();
 }
 
+/**
+ * @brief Read Control and Status Register1
+ * @return data : Read Data
+ */
 uint8_t FaBoRTC_PCF2129::readCtrl(void) {
   return readI2c(PCF2129_CONTROL_REGISTERS);
 }
 
+/**
+ * @brief Write Control and Status Register1
+ * @param [in] data : Write Data
+ */
 void FaBoRTC_PCF2129::writeCtrl(uint8_t data) {
   writeI2c(PCF2129_CONTROL_REGISTERS, data);
 }
--- src/FaBoRTC_PCF2129.h
+++ src/FaBoRTC_PCF2129.h
@@ -1,69 +1,119 @@
+/**
+ * @file  FaBoRTC_PCF2129.h
+ * @brief fabo libtary of PCF2129
+ * @author Akira Sasaki
+ * @date 2,10, 2016
+ */
 #ifndef FABORTC_PCF2129_H
-#define FABORTC_PCF2129_H
+  #define FABORTC_PCF2129_H
 
-#include <Arduino.h>
-#include <Wire.h>
+  #include <Arduino.h>
+  #include <Wire.h>
 
-#define PCF2129_SLAVE_ADDRESS 0x51
+  /** SLAVE_ADDRESS */
+  #define PCF2129_SLAVE_ADDRESS 0x51
 
-#define PCF2129_CONTROL_REGISTERS 0x00
-#define PCF2129_CONTROL_12_24 0x04
+  /** Cntrol Register */
+  #define PCF2129_CONTROL_REGISTERS 0x00
+  /** Mode Cntrol Register : 12-hour mode or 24-hour mode */
+  #define PCF2129_CONTROL_12_24 0x04
 
-#define PCF2129_SECONDS 0x03
-#define PCF2129_MINUTES 0x04
-#define PCF2129_HOURS 0x05
-#define PCF2129_DAYS 0x06
-#define PCF2129_WEEKDAYS 0x07
-#define PCF2129_MONTHS 0x08
-#define PCF2129_YEARS 0x09
+  /** Seconds Register */
+  #define PCF2129_SECONDS 0x03
+  /** Minutes Register */
+  #define PCF2129_MINUTES 0x04
+  /** Hours Register */
+  #define PCF2129_HOURS 0x05
+  /** Days Register */
+  #define PCF2129_DAYS 0x06
+  /** Weekdays Register */
+  #define PCF2129_WEEKDAYS 0x07
+  /** Months Register */
+  #define PCF2129_MONTHS 0x08
+  /** Years Register */
+  #define PCF2129_YEARS 0x09
 
+  /**
+   * @class DateTime
+   * @brief Get Date Time
+   */
+  class DateTime {
+    public:
+      /** Date Time */
+      DateTime (uint16_t years, uint8_t months, uint8_t days,
+                uint8_t hours, uint8_t minutes, uint8_t seconds);
+      /**
+       * @brief Return Year
+       * @return uint16_t : year + 2000
+       */
+      uint16_t year()  const { return y+2000; }
+      /**
+       * @brief Return Month
+       * @return uint8_t : month
+       */
+      uint8_t month()  const { return m; }
+      /**
+       * @brief Return day
+       * @return uint8_t : day
+       */
+      uint8_t day()    const { return d; }
+      /**
+       * @brief Return hour
+       * @return uint8_t : hour
+       */
+      uint8_t hour()   const { return hh; }
+      /**
+       * @brief Return minute
+       * @return uint8_t : minute
+       */
+      uint8_t minute() const { return mm; }
+      /**
+       * @brief Return Second
+       * @return uint8_t : second
+       */
+      uint8_t second() const { return ss; }
+    protected:
+      uint8_t y,m,d,hh,mm,ss;
+  };
 
-class DateTime {
-  public:
-    DateTime (uint16_t years, uint8_t months, uint8_t days,
-              uint8_t hours, uint8_t minutes, uint8_t seconds);
-    uint16_t year()  const { return y+2000; }
-    uint8_t month()  const { return m; }
-    uint8_t day()    const { return d; }
-    uint8_t hour()   const { return hh; }
-    uint8_t minute() const { return mm; }
-    uint8_t second() const { return ss; }
-  protected:
-    uint8_t y,m,d,hh,mm,ss;
-};
+  /**
+   * @class FaBoRTC_PCF2129
+   * @brief PCF2129 Control
+   */
+  class FaBoRTC_PCF2129 {
+    public:
+      FaBoRTC_PCF2129(uint8_t addr = PCF2129_SLAVE_ADDRESS);
+      bool searchDevice(void);
+      void configure(void);
+      uint8_t getSeconds(void);
+      void setSeconds(uint8_t seconds);
+      uint8_t getMinutes(void);
+      void setMinutes(uint8_t minutes);
+      uint8_t getHours(void);
+      void setHours(uint8_t hours);
+      uint8_t getDays(void);
+      void setDays(uint8_t days);
+      uint8_t getWeekdays(void);
+      void setWeekdays(uint8_t weekdays);
+      uint8_t getMonths(void);
+      void setMonths(uint8_t months);
+      uint8_t getYears(void);
+      void setYears(uint8_t years);
+      DateTime now(void);
+      void setDate(uint16_t years, uint8_t months, uint8_t days,
+                   uint8_t hours, uint8_t minutes, uint8_t seconds);
+      void set12mode(void);
+      void set24mode(void);
+    private:
+      uint8_t _i2caddr;
 
-class FaBoRTC_PCF2129 {
-  public:
-    FaBoRTC_PCF2129(uint8_t addr = PCF2129_SLAVE_ADDRESS);
-    bool searchDevice(void);
-    void configure(void);
-    uint8_t getSeconds(void);
-    void setSeconds(uint8_t seconds);
-    uint8_t getMinutes(void);
-    void setMinutes(uint8_t minutes);
-    uint8_t getHours(void);
-    void setHours(uint8_t hours);
-    uint8_t getDays(void);
-    void setDays(uint8_t days);
-    uint8_t getWeekdays(void);
-    void setWeekdays(uint8_t weekdays);
-    uint8_t getMonths(void);
-    void setMonths(uint8_t months);
-    uint8_t getYears(void);
-    void setYears(uint8_t years);
-    DateTime now(void);
-    void setDate(uint16_t years, uint8_t months, uint8_t days,
-                 uint8_t hours, uint8_t minutes, uint8_t seconds);
-    void set12mode(void);
-    void set24mode(void);
-  private:
-    uint8_t _i2caddr;
-    uint8_t bcdToDec(uint8_t value);
-    uint8_t decToBcd(uint8_t value);
-    uint8_t readI2c(uint8_t address);
-    void writeI2c(uint8_t address, uint8_t data);
-    uint8_t readCtrl(void);
-    void writeCtrl(uint8_t data);
-};
+      uint8_t bcdToDec(uint8_t value);
+      uint8_t decToBcd(uint8_t value);
+      uint8_t readI2c(uint8_t address);
+      void writeI2c(uint8_t address, uint8_t data);
+
+      uint8_t readCtrl(void);
+      void writeCtrl(uint8_t data);
+  };
 
 #endif // FABORTC_PCF2129_H
